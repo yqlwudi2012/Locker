bbe4aa098bfbbfd0baae1ed56a5db153dd0eedb7
Sejta <412269866@qq.com>
2018-05-28 13:54:37 +0800

    修正druid 连接池 添加logback日志输出 添加Interceptor拦截器token验证 添加Redis对token的存储和校验

--- src/main/java/com/locker/controller/AuthorizationController.java
+++ src/main/java/com/locker/controller/AuthorizationController.java
@@ -2,9 +2,11 @@ package com.locker.controller;
 
 import com.locker.bean.TokenBean;
 import com.locker.bean.WeTokenBean;
+import com.locker.service.RedisService;
 import com.locker.service.weChatService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,16 +22,19 @@ public class AuthorizationController {
 
     @Autowired
     private weChatService weChatService;
+    @Autowired
+    private RedisService redisService;
 
     @PostMapping
     @RequestMapping(value = "/{code}")
     public TokenBean userAuthor(@PathVariable(value = "code",required = true) String code){
         System.out.println("new Connect come in..:"+code);
         WeTokenBean weTokenBean=weChatService.authUser(code);
-        System.out.println(weTokenBean.toString());
+        if(!StringUtils.isEmpty(weTokenBean.getSession_key()))
+        redisService.setTokenToRedisTemporaryHours(weTokenBean,(long)2);
+
         TokenBean tokenBean=new TokenBean();
         tokenBean.setAccess_token(weTokenBean.getSession_key());
-        //redis token+userid 2h
         return tokenBean;
     }
 
